cf6bc50008a22ec4f66e9ae7bb1d3784251b02e8
Austin <austin.fratesi@gmail.com>
2025-09-26 14:21:57 -0500

    latest update

--- src/App.jsx
+++ src/App.jsx
@@ -56,7 +56,6 @@ function App() {
             </Route>
           </Route>
 
-
           <Route path='*' element={<PageNotFound />}/>
         </Route>
       </Routes>
--- src/components/Host/HostVanDetailsNav/Pricing.jsx
+++ src/components/Host/HostVanDetailsNav/Pricing.jsx
@@ -2,7 +2,7 @@ import {useOutletContext} from 'react-router-dom'
 
 export default function Pricing() {
   const data = useOutletContext();
-  // console.log(data)
+  //console.log(data)
   return (
     <h1>${data.price}/day</h1>
   )
